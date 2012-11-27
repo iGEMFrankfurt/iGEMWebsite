e253b03ce3a494601912703c13516c434c8e3953
OpenShift guest <seb.roy@t-online.de>
2012-11-27 08:56:25 -0500

    print

--- javatest/hellocloud.java
+++ javatest/hellocloud.java
@@ -1,6 +1,7 @@
 
 public class hellocloud {
-    public static void main(String[] args){
+    public static void main(Strg[] args){
         System.out.println("hello world");
+        System.out.println("print");
     }
 }
