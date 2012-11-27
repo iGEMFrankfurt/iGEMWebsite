652873f391f8f9630c4b06bf04bc3d4b97c2824f
RoyalSWiSH <seb.roy@t-online.de>
2012-11-27 01:17:05 +0100

    iGEM Frankfurt Website
    Website is in static folder. There is no scala active right now.

---
+++ src/main/scala/code/snippet/HelloWorld.scala
@@ -0,0 +1,24 @@
+package code 
+package snippet 
+
+import scala.xml.{NodeSeq, Text}
+import net.liftweb.util._
+import net.liftweb.common._
+import java.util.Date
+import code.lib._
+import Helpers._
+
+class HelloWorld {
+  lazy val date: Box[Date] = DependencyFactory.inject[Date] // inject the date
+  lazy val testout: Box[Date] = DependencyFactory.inject[Date]
+  // replace the contents of the element with id "time" with the date
+  def howdy = "#time *" #> date.map(_.toString)
+  def howdy2 = "#top *" #> date.map(_.toString)
+
+  /*
+   lazy val date: Date = DependencyFactory.time.vend // create the date via factory
+
+   def howdy = "#time *" #> date.toString
+   */
+}
+
